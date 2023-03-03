1814fb5916e100fd8c0a21181f2dae773788e9c6
LilAliTech <lilalix20@gmail.com>
2023-03-03 23:32:51 +0000

    Main

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = ({heading, message}) => {
             <div className='p-5 text-white z-[2] ml-[-10rem] mt-[-10rem]'>
                 <h2 className='text-5xl font-bold'>{heading}</h2>
                 <p className='py-5 text-xl'>{message}</p>
-                <button className='px-8 py-2 border'>Learn more</button>
+                <button className='px-8 py-2 border hover:bg-blue-500 '>Learn more</button>
                 
             </div>
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,11 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai';
 
 function Navbar() {
   return (
-    <div>
-        <div>
+    <div className='fixed left-0 top-0 w-full z-10 ease-in duration-300'>
+        <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white'>
             <link href='/'></link>
             <h1>MQ</h1>
             <ul>
@@ -12,7 +13,7 @@ function Navbar() {
                     <Link href='/'>Home</Link>
                 </li>
                 <li>
-                    <Link href='/'>About</Link>
+                    <Link href='/#about'>About</Link>
                 </li>
                 <li>
                     <Link href='/'>Projects</Link>
@@ -24,6 +25,33 @@ function Navbar() {
                     <Link href='/'>Contact</Link>
                 </li>
             </ul>
+          {/* Mobile Button */}
+
+          <div>
+            <AiOutlineMenu size={20} />
+          </div>
+          
+          {/* Mobile Menu */}
+          <div>
+          <ul>
+                <li>
+                    <Link href='/'>Home</Link>
+                </li>
+                <li>
+                    <Link href='/#about'>About</Link>
+                </li>
+                <li>
+                    <Link href='/'>Projects</Link>
+                </li>
+                <li>
+                    <Link href='/'>Services</Link>
+                </li>
+                <li>
+                    <Link href='/'>Contact</Link>
+                </li>
+            </ul>
+          </div>
+
         </div>
     </div>
   )
